d20842ffd49458b9f3215f372955d3c515fa9bb8
briartedesco <88670876+briartedesco@users.noreply.github.com>
2021-10-01 18:40:51 -0400

    edited css

---
+++ src/index.js
@@ -0,0 +1,57 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("#current-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function search(city) {
+  let apiKey = "cd173a006b0e51dac58c6d8064c94178";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-weather").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#find-city-value").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#find-city");
+searchForm.addEventListener("submit", handleSearch);
+
+search("Toronto");
